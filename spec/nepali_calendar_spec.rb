54a6df2c0e7ea7a079019aad5dade18bd2553e4b
Lal B. Saud <lalusaud@gmail.com>
2015-09-09 22:51:15 -0400

    Add Calendar class

--- spec/nepali_calendar_spec.rb
+++ spec/nepali_calendar_spec.rb
@@ -6,12 +6,12 @@ describe NepaliCalendar do
   end
 
   it 'converts date from ad_to_bs' do
-    expect(NepaliCalendar.ad_to_bs('2015', '09', '09')).to eq(Date.parse('2072-05-23'))
-    expect(NepaliCalendar.ad_to_bs('2010', '04', '16')).to eq(Date.parse('2067-01-03'))
+    expect(NepaliCalendar::Calendar.ad_to_bs('2015', '09', '09')).to eq(Date.parse('2072-05-23'))
+    expect(NepaliCalendar::Calendar.ad_to_bs('2010', '04', '16')).to eq(Date.parse('2067-01-03'))
   end
 
   it 'converts date from bs_to_ad' do
-    expect(NepaliCalendar.bs_to_ad('2072', '05', '23')).to eq(Date.parse('2015-09-09'))
-    expect(NepaliCalendar.bs_to_ad('2067', '01', '03')).to eq(Date.parse('2010-04-16'))
+    expect(NepaliCalendar::Calendar.bs_to_ad('2072', '05', '23')).to eq(Date.parse('2015-09-09'))
+    expect(NepaliCalendar::Calendar.bs_to_ad('2067', '01', '03')).to eq(Date.parse('2010-04-16'))
   end
 end
